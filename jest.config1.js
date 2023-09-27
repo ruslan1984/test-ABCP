0e94afd6f16fc8641c02cb8a630c746d450a505d
Ruslan <ruslan231984@yandex.ru>
2023-09-28 00:39:42 +0300

    remove tests

--- jest.config1.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // testMatch: ["**/*.test.(ts|js|tsx|jsx)"],
-  // moduleDirectories: ["node_modules", "src"],
-  // preset: "ts-jest",
-  // transformIgnorePatterns: ["node_modules/(?!(sucrase)/)"],
-  // transform: {
-  //   "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-  // },
-};
